96d726576a25564251c08f9319e6526ff12d570e
Jay Prakash Singh <jp213@ymail.com>
2014-01-15 19:01:45 +0530

    Update middlewares.py

--- flipscrapy/flipscrapy/middlewares.py
+++ flipscrapy/flipscrapy/middlewares.py
@@ -16,6 +16,6 @@ class ProxyMiddleware(object):
     def process_request(self, request, spider):
         ip_port = choice(ip_list)
         request.meta['proxy'] = "http://"+ip_port
-        proxy_user_pass = 'vinku:india123'
+        proxy_user_pass = 'xxxx:xxxx'
         encoded_user_pass = base64.encodestring(proxy_user_pass)
         request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
